3d648273a958f76837e581e9f8ce955c66e301d5
Abdul Ahad Abid <ch.abdulsahad@gmail.com>
2026-03-05 00:53:32 +0000

    Added librarian menu: add, remove books and overdue report

--- Librarian.cpp
+++ Librarian.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 #include "Librarian.h"
+#include "Library.h"
+#include "Book.h"
 using namespace std;
 
 Librarian::Librarian(string u, string p) : User(u, p) {}
 
 void Librarian::menu(Library &library) {
-    cout << "\nLibrarian menu\n";
+    int choice;
+    string input, author;
+    do {
+        cout << "\n--- LIBRARIAN MENU ---\n";
+        cout << "1. Add Book\n";
+        cout << "2. Remove Book\n";
+        cout << "3. Overdue Report (placeholder)\n";
+        cout << "0. Logout\n";
+        cin >> choice;
+        cin.ignore();
+
+        if(choice == 1) {
+            cout << "Title: "; getline(cin, input);
+            cout << "Author: "; getline(cin, author);
+            library.addBook(Book(input, author));
+            cout << "Book added successfully.\n";
+        }
+        else if(choice == 2) {
+            cout << "Title to remove: "; getline(cin, input);
+            library.removeBook(input);
+            cout << "Book removed successfully.\n";
+        }
+        else if(choice == 3) {
+            library.showOverdueReport();
+        }
+
+    } while(choice != 0);
 }
